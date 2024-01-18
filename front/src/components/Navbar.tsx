989fc8489710faf2bd11ee87148d8cb8dbbe996d
Louis Teilliais <louisteilliais@MacBook-Pro-de-Louis.local>
2024-01-18 11:22:29 +0100

    init

---
+++ front/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import { css } from '../styled-system/css';
+
+
+export const Navbar = () => {
+    return (
+        <div>
+            <div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼!</div>
+        </div>
+    )
+}
