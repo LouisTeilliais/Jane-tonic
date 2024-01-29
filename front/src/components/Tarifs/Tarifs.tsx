20318af015a55bc6efef4f9bc1bcb6a5f869a935
Luke <lukebryan@orange.fr>
2024-01-29 23:29:41 +0100

    Ajout style de la page + problème liste avec la séance et prix + mettre les titre en gras

--- front/src/components/Tarifs/Tarifs.tsx
+++ front/src/components/Tarifs/Tarifs.tsx
@@ -2,36 +2,36 @@ import './Tarifs.css';
 
 export default function Tarifs() {
     return (
-        <>
-            <div>
-                <h2>Cours collectif 1h</h2>
+        <div className='bloc-tarifs'>
+            <div className="cours-collec-1h">
+                <h2 className="titre">Cours collectif 1h</h2>
                 <div>
-                    <h3>Circuit training fitness & Streching</h3>
-                    <ul>
-                        <li>1 séance 12 euros</li>
-                        <li>3 séances 30 euros</li>
-                        <li>10 séances 90 euros</li>
-                    </ul>
+                    <h3 id="titre-circuit">Circuit training fitness & Streching</h3>
+                    <ul id="liste-prix-séances-circuit">
+                        <li  id="prix-séance-collective-circuit"><span>1 séance</span><strong> 12 euros</strong></li>
+                        <li  id="prix-séance-collective-circuit"><span>3 séances</span><strong> 30 euros</strong></li>
+                        <li  id="prix-séance-collective-circuit"><span>10 séances</span><strong> 90 euros</strong></li>
+                    </ul >
                     <p></p>
-                    <h3>Pilates</h3>
-                    <ul>
-                        <li>1 séance 15 euros</li>
-                        <li>3 séances 40 euros</li>
-                        <li>10 séances 125 euros</li>
+                    <h3 id="titre-pilates">Pilates</h3>
+                    <ul id="liste-prix-séances-pilates">
+                        <li  id="prix-séance-collective-pilates"><span>1 séance</span> <strong>15 euros</strong></li>
+                        <li  id="prix-séance-collective-pilates"><span>3 séances</span> <strong>40 euros</strong></li>
+                        <li  id="prix-séance-collective-pilates"><span>10 séances</span> <strong>125 euros</strong></li>
                     </ul>
                 </div>
             </div>
-            <div>
-                <h2>Cours particulier domicile 1h</h2>
+            <div className="cours-partic-dom-1h">
+                <h2 className="titre">Cours particulier domicile 1h</h2>
                 <div>
-                    <h3>Circuit training fitness, Streching & pilates</h3>
-                    <ul>
-                        <li>1 séance SOLO 50 euros</li>
-                        <li>1 séance DUO 70 euros</li>
+                    <h4 id="titre-circuit">Circuit training fitness, Streching & pilates</h4>
+                    <ul id="liste-prix-séances">
+                        <li id="prix-séance-domicile" ><span>1 séance SOLO</span> <strong>50 euros</strong></li>
+                        <li id="prix-séance-domicile"><span>1 séance DUO</span> <strong>70 euros</strong></li>
                     </ul>
-                    <h3>Les cours particuliers à domicile peuvent être réglés en SESU, vous bénéficierez d'une réduction 50% de la totalité du montant sur votre prochaine échéance d'impôts.</h3>
+                    <h5 id="reglement-domicile">Les cours particuliers à domicile peuvent être réglés en SESU, vous bénéficierez d'une réduction 50% de la totalité du montant sur votre prochaine échéance d'impôts.</h5>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
