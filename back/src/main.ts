ca768691a564a495c47323b054600157cde010a8
Louis Teilliais <louis.teilliais@gmail.com>
2024-11-19 16:47:11 +0100

    update front

--- back/src/main.ts
+++ back/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api')
 
   app.enableCors({
-    origin: ['http://localhost:5173'], // URL FRONT
+    origin: ['http://localhost:5173, "https://janetonic.fr'], // URL FRONT
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
   });
