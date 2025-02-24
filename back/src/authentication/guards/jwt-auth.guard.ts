c4c3c4df1a284571f567e0836264e6544c3b801b
Louis Teilliais <louis.teilliais@gmail.com>
2025-02-24 19:41:34 +0100

    fix responsive mobile

--- back/src/authentication/guards/jwt-auth.guard.ts
+++ back/src/authentication/guards/jwt-auth.guard.ts
@@ -1,7 +1,16 @@
-
-import { Injectable } from '@nestjs/common';
-import {AuthGuard} from '@nestjs/passport';
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+    canActivate(context: ExecutionContext) {
+        const request = context.switchToHttp().getRequest();
+        
+        // Autoriser les requêtes OPTIONS pour éviter les problèmes CORS
+        if (request.method === 'OPTIONS') {
+            return true;
+        }
+
+        return super.canActivate(context);
+    }
 }
