41abd38963b67f12f3e0273439e3335230d80202
Louis Teilliais <louis.teilliais@gmail.com>
2025-03-11 19:26:06 +0100

    fix

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,27 +4,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  app.setGlobalPrefix('api')
+
+  app.setGlobalPrefix('api');
 
   app.enableCors({
-    // origin: ['http://localhost:5173'], // URL FRONT
-    origin: ['https://janetonic.fr/', 'https://api.janetonic.fr/'], 
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
+    origin: ['https://janetonic.fr', 'https://api.janetonic.fr'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(204);
-    }
-    next();
-  });
-  
- 
   const config = new DocumentBuilder()
     .setTitle('Jane Tonic')
     .setDescription('The Jane Tonic API description')
@@ -32,10 +20,9 @@ async function bootstrap() {
     .addTag('Api')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  const options = new DocumentBuilder().addBearerAuth()
+  const options = new DocumentBuilder().addBearerAuth();
   SwaggerModule.setup('Api', app, document);
 
-  
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
