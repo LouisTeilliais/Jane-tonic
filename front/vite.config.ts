527ee8594a92c0e8ae33686d600fdc994a0acabb
Louis Teilliais <louis.teilliais@gmail.com>
2025-04-02 20:36:39 +0200

    fix

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -6,6 +6,14 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'import.meta.env.VITE_APP_PORT': JSON.stringify(process.env.BASE_URL)
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://api.janetonic.fr', // ou votre URL backend en local comme 'http://localhost:5001'
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
-
